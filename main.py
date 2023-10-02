19f7c77213f4c433901eba51a4514caf04e00c92
annelija <92726236+andromeda44azora@users.noreply.github.com>
2023-10-02 14:44:52 +0300

    asasas
    asasasas

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from Flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask('app')
 
 @app.route('/')
 def sakums():
-    return "jsjsadjksdfjds"
+    return render_template('index.html')
+
+@app.route('/sveiki')
+def sveiki():
+    visi_vardi = ['lapsene', 'Katrīne', 'pasaule']
+    return render_template('sveiki.html', vards = visi_vardi)
 
 if __name__ == '__main__':
     app.run(threaded=True, port = 5000)
